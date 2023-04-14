85a002fe60b632c6ad7d26a14701360baf3f421f
nao <xxxxx@gmail.com>
2023-04-14 23:42:25 +0900

    add eslint setting

--- app/src/__tests__/components/sample.test.tsx
+++ app/src/__tests__/components/sample.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
 import TextDisplay from '@/components/sample';
 import '@testing-library/jest-dom';
 
