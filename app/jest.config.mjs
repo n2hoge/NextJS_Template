f722c7fd1942a54ab7cd5a645371d1800ffb65a5
nao <xxxxx@gmail.com>
2023-04-15 19:44:14 +0900

    fix eslint error

--- app/jest.config.mjs
+++ app/jest.config.mjs
@@ -1,9 +1,9 @@
 // jest.config.mjs
-import nextJest from "next/jest.js";
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  dir: './',
 });
 
 // Add any custom config to be passed to Jest
@@ -11,21 +11,9 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  preset: "ts-jest",
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json",
-    },
-  },
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/**/**/?(*.)+(spec|test).+(ts|tsx|js)"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1", // 追加
-  },
-  testEnvironment: "jest-environment-jsdom",
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/**/**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testEnvironment: 'jest-environment-jsdom',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
